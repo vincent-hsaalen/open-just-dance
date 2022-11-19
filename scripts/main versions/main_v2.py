2af9ccabab0398517f20326c1ac1e1cf2a063e69
vincent-hsaalen <vincent.burkhardt@studmail.htw-aalen.de>
2022-11-19 16:35:52 +0100

    added main backups

--- scripts/main versions/main_v2.py
+++ scripts/main versions/main_v2.py
@@ -51,15 +51,35 @@ def main():
     L_11_24, L_11_22, L_22_23, L_14_19, L_14_21, L_19_20, L_1_8]
 
     # print all body parts and their coordinates
-    # print(printBodyPartsAndCoordinatesForDebuggingForOneFile(jsonfiles, 0))
+    print(printBodyPartsAndCoordinatesForDebuggingForOneFile(jsonfiles, 0))
     
-    # prints lists of all slopes for every line
-    # print(returnSlopes(POINTS_LIST))
+    # calculate slopes
+    SLOPES: list[np.float64] = [calculateSlope(POINTS_LIST[c][0][0], POINTS_LIST[c][0][1], POINTS_LIST[c][1][0], POINTS_LIST[c][1][1]) for c in range(0, len(POINTS_LIST))]
+    print(f"\n--- SLOPES ---\n{SLOPES}")
+    
+    # --- calculate important angles --- #
+    
+    BODY: np.float64 = calculateAngle(SLOPES[0], SLOPES[23], 0)
+    ARM_RIGHT: np.float64 = calculateAngle(SLOPES[7], SLOPES[8], 1)
+    ARM_LEFT: np.float64 = calculateAngle(SLOPES[9], SLOPES[10], 2)
+    SHOULDER_RIGHT: np.float64 = calculateAngle(SLOPES[5], SLOPES[7] ,3)
+    SHOULDER_LEFT: np.float64 = calculateAngle(SLOPES[6], SLOPES[9], 4)
+    LEG_RIGHT: np.float64 = calculateAngle(SLOPES[13], SLOPES[14], 5)
+    LEG_LEFT: np.float64 = calculateAngle(SLOPES[15], SLOPES[16], 6)
+    HIP_RIGHT: np.float64 = calculateAngle(SLOPES[11], SLOPES[13], 7)
+    HIP_LEFT: np.float64 = calculateAngle(SLOPES[12], SLOPES[15], 8)
 
-    # prints list of all angles (in degrees)
-    print(returnAngles(returnSlopes(POINTS_LIST)))
+    print("\n--- ANGELS ---")
+    print(f"BODY: {BODY}°")
+    print(f"ARM_RIGHT: {ARM_RIGHT}°")
+    print(f"ARM_LEFT: {ARM_LEFT}°")
+    print(f"SHOULDER_RIGHT: {SHOULDER_RIGHT}°")
+    print(f"SHOULDER_LEFT: {SHOULDER_LEFT}°")
+    print(f"LEG_RIGHT: {LEG_RIGHT}°")
+    print(f"LEG_LEFT: {LEG_LEFT}°")
+    print(f"HIP_RIGHT: {HIP_RIGHT}°")
+    print(f"HIP_LEFT: {HIP_LEFT}°")
 
-    # --- runtime end --- #
     endtime = time.time()
     elapsed_time = endtime - starttime
     print(f'Execution time: {elapsed_time:.2} seconds')
@@ -109,8 +129,7 @@ def calculateAngle(m1: np.float64, m2: np.float64, body_id:int) -> (np.float64 |
     return np.round_(angle_gr, decimals=2)
 
 # calculates the slope of two points by using the formula: m = (y2-y1)/(x2-x1)
-def calculateSlope(x1: np.float64, y1: np.float64, x2: np.float64, y2: np.float64) -> np.float64: 
-    return np.round_((y2 - y1) / (x2 - x1), decimals=2)
+def calculateSlope(x1: np.float64, y1: np.float64, x2: np.float64, y2: np.float64) -> np.float64: return np.round_((y2 - y1) / (x2 - x1), decimals=2)
 
 # only for debugging
 def printBodyPartsAndCoordinatesForDebuggingForOneFile(jsonfiles, index):
@@ -122,45 +141,6 @@ def printBodyPartsAndCoordinatesForDebuggingForOneFile(jsonfiles, index):
     print("\n--- body parts and their coordinates ---")
     for count, coordinates in enumerate(returnListofTuples(jsonfiles, index)): 
         print(f'{count}. {parts[count]}: {coordinates}')
-
-# returns list of slopes --> [m1, m2, m3, ...] input: POINTS_LIST
-def returnSlopes(listofpoints):
-    # calculate slopes
-    slopes: list[np.float64] = [calculateSlope(listofpoints[c][0][0], listofpoints[c][0][1], listofpoints[c][1][0], listofpoints[c][1][1]) for c in range(0, len(listofpoints))]
-
-    # print all slopes for debugging
-    # print(f"\n--- SLOPES ---\n{slopes}")
-
-    return slopes
-    
-# returns list of angles --> [a1, a2, a3, ...] input: SLOPES
-def returnAngles(listOfSlopes):
-    BODY: np.float64 = calculateAngle(listOfSlopes[0], listOfSlopes[23], 0)
-    ARM_RIGHT: np.float64 = calculateAngle(listOfSlopes[7], listOfSlopes[8], 1)
-    ARM_LEFT: np.float64 = calculateAngle(listOfSlopes[9], listOfSlopes[10], 2)
-    SHOULDER_RIGHT: np.float64 = calculateAngle(listOfSlopes[5], listOfSlopes[7] ,3)
-    SHOULDER_LEFT: np.float64 = calculateAngle(listOfSlopes[6], listOfSlopes[9], 4)
-    LEG_RIGHT: np.float64 = calculateAngle(listOfSlopes[13], listOfSlopes[14], 5)
-    LEG_LEFT: np.float64 = calculateAngle(listOfSlopes[15], listOfSlopes[16], 6)
-    HIP_RIGHT: np.float64 = calculateAngle(listOfSlopes[11], listOfSlopes[13], 7)
-    HIP_LEFT: np.float64 = calculateAngle(listOfSlopes[12], listOfSlopes[15], 8)
-
-    listOfAngles = [BODY, ARM_RIGHT, ARM_LEFT, SHOULDER_RIGHT, SHOULDER_LEFT, LEG_RIGHT, LEG_LEFT, HIP_RIGHT, HIP_LEFT]
-
-    # print all angles for debugging
-
-    # print("\n--- ANGELS ---")
-    # print(f"BODY: {BODY}°")
-    # print(f"ARM_RIGHT: {ARM_RIGHT}°")
-    # print(f"ARM_LEFT: {ARM_LEFT}°")
-    # print(f"SHOULDER_RIGHT: {SHOULDER_RIGHT}°")
-    # print(f"SHOULDER_LEFT: {SHOULDER_LEFT}°")
-    # print(f"LEG_RIGHT: {LEG_RIGHT}°")
-    # print(f"LEG_LEFT: {LEG_LEFT}°")
-    # print(f"HIP_RIGHT: {HIP_RIGHT}°")
-    # print(f"HIP_LEFT: {HIP_LEFT}°")
-
-    return listOfAngles
 
 if __name__ == "__main__":
     main()
